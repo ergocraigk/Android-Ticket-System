6badec4c0dfc141e333692000072b0c611acb797
ergocraigk <ergocraigk@gmail.com>
2016-05-02 10:04:53 -0700

    skeleton for jsonobject request, second activity

--- MyApplication2/app/src/main/java/com/example/zeus/androidgrticketsystem/AppLanding.java
+++ MyApplication2/app/src/main/java/com/example/zeus/androidgrticketsystem/AppLanding.java
@@ -1,17 +1,25 @@
 package com.example.zeus.androidgrticketsystem;
 
+
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONObject;
 
 
 public class AppLanding extends AppCompatActivity {
@@ -22,6 +30,11 @@ public class AppLanding extends AppCompatActivity {
     TextView successOrFailMessage;
 
     public final static String EXTRA_MESSAGE = "this is the extra message we had to have for some reason.";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +51,44 @@ public class AppLanding extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent loginSuccessIntent = new Intent(AppLanding.this, openTicketsActivity.class);
-                String message = userName.getText().toString();
-                loginSuccessIntent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(loginSuccessIntent);
+
+                JSONObject params = new JSONObject();
+                //params.put("username","rajesh@gmail.com");
+                //params.put("password","hellothere");
+
+                JsonObjectRequest loginRequest = new JsonObjectRequest(
+                        DownloadManager.Request.Method.POST,
+                        "http://192.168.2.67/tmp/test.php",
+                        params,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject jsonObject) {
+                                Log.d("","");
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                Log.d("", "");
+                            }
+                        });
+
+
+                requestQueue.add(loginRequest);
+
+
+                if (successOrFailMessage.getText() == "true") {
+                    Intent loginSuccessIntent = new Intent(AppLanding.this, openTicketsActivity.class);
+                    String message = userName.getText().toString();
+                    loginSuccessIntent.putExtra(EXTRA_MESSAGE, message);
+                    startActivity(loginSuccessIntent);
+                }
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -66,5 +111,45 @@ public class AppLanding extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AppLanding Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zeus.androidgrticketsystem/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AppLanding Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zeus.androidgrticketsystem/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
